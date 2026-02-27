7256cbd98c0ecea9a2d527cd55a8780b6b995b5c
zenkizero99 <135101316+zenkizero99@users.noreply.github.com>
2026-02-27 17:17:52 +0900

    Update list.js

--- api/list.js
+++ api/list.js
@@ -1,10 +1,16 @@
-import { kv } from '@vercel/kv';
+import { kv } from "@vercel/kv";
 
 export default async function handler(req, res) {
 
-  const data = await kv.lrange('participants', 0, -1);
+  try {
 
-  const parsed = data.map(item => JSON.parse(item));
+    const data = await kv.lrange("participants", 0, -1);
 
-  res.status(200).json(parsed.reverse());
+    const parsed = data.map(item => JSON.parse(item));
+
+    return res.status(200).json(parsed.reverse());
+
+  } catch (err) {
+    return res.status(500).json({ error: "Server error" });
+  }
 }
